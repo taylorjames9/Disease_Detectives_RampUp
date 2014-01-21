d118a60f618ab4938edae8a34b23b96f58763fe1
taylorjames9 <taylorjames9@gmail.com>
2014-01-21 14:28:08 -0600

    Working but can't tell if everyone is infected.

--- Assets/scripts/Cube_Party_Script.cs
+++ Assets/scripts/Cube_Party_Script.cs
@@ -4,61 +4,104 @@ using System.Collections;
 public class Cube_Party_Script : MonoBehaviour {
 
 
-	enum MyState {ChattingContent, ImBored};
+	enum MyState {ChattingContent, ImBored, ConvoW_Infected};
 
 
 	public GameObject[] NPCArray = {}; 
-	GameObject target;
+	public GameObject target;
 	MyState myState;
 	float boredTimer;
-	//bool moving = true;
 	bool needTarget = true;
-
-
-
+	public bool infected;
+	public int susceptibility;
+	public Cube_Party_Script targetScript;
+	public bool imaMover;
 
 	// Use this for initialization
 	void Start () {
+		//print (gameObject.name);
+		if (this.gameObject.name == "Guest_4" || this.gameObject.name == "Guest_6") {
+			imaMover = true;
+			//print ("I've been promoted to a mover.");
+		}
 
 		myState = MyState.ChattingContent;
 		boredTimer = Time.time + Random.Range (3, 5);
-
+		infected = RandomBoolean ();
+		susceptibility = Random.Range (0, 6);
+		if (infected) {
+			transform.renderer.material.color = Color.red; 
+			print ("I'm infected");
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		switch (myState) {
 		case MyState.ChattingContent:
-			if (boredTimer <= Time.time) {
-				boredTimer = Time.time + Random.Range (3, 10);
-				myState = MyState.ImBored;
-				needTarget = true;
+			if (imaMover) {
+				GettingBored ();
+				GettingInfected ();
+			} else {
+				//Dude_cameUpNow_ImInfected ();
 			}
 			break;
 
 		case MyState.ImBored:
 			SwitchGroup ();
 			break;
-
 		default:
 			break;
 		}
 	}
 
+
 	void SwitchGroup(){
+
+			transform.LookAt (target.transform);
+
+			if (Vector3.Distance (transform.position, target.transform.position) > 2) {
+				transform.position = Vector3.Lerp (transform.position, target.transform.position, 0.01f);
+			} else if (Vector3.Distance (transform.position, target.transform.position) <= 2) {
+				myState = MyState.ChattingContent;
+			}
+	}
+
+	bool RandomBoolean(){
+		return (Random.value > 0.5f);
+	}
+
+	void GettingBored(){
+		if (boredTimer <= Time.time) {
+			boredTimer = Time.time + Random.Range (3, 10);
+			myState = MyState.ImBored;
+			needTarget = true;
+		}
 		if (needTarget) {
 			target = NPCArray [(Random.Range (0, 4))];
 			needTarget = false;
 		}
-
-		transform.LookAt (target.transform);
-
-		if (Vector3.Distance (transform.position, target.transform.position) > 2) {
-			transform.position = Vector3.Lerp (transform.position, target.transform.position, 0.01f);
-		}
-		else if (Vector3.Distance (transform.position, target.transform.position) <= 2) {
-			myState = MyState.ChattingContent;
-		}
-
 	}
+
+	void GettingInfected(){
+
+		if (!infected){
+		targetScript = target.GetComponent<Cube_Party_Script>();
+			if (targetScript.infected && susceptibility > 0) {
+				infected = true;
+				print ("I got infected");
+				transform.renderer.material.color = Color.red; 
+			}
+		}
+	}
+	
+				/*void Dude_cameUpNow_ImInfected{
+					if(Vector3.Distance(gameObject("Guest_4")<2.1 && gameObject("Guest_4"). ){
+
+					}
+	
+	}*/
+
+
+
 }
---
+++ Assets/scripts/Party_Behaviors.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Party_Behaviors : MonoBehaviour {
+
+
+	enum MyState {Chatting, Moving, Bored};
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		switch (MyState) {
+		case MyState.Chatting:
+		
+			break;
+		
+		case MyState.Bored:
+
+			break;
+		
+		case MyState.Moving:
+
+
+			break;
+
+
+
+		}
+
+	
+	}
+}
